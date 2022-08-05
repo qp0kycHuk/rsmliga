b84be3eb8cc071e4954933876d215201048e4224
qp0kycHuk <serega990112@mail.ru>
2022-08-05 10:27:11 +0300

    add include html, add generator html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const CopyPlugin = require("copy-webpack-plugin");
 const path = require('path');
+const fs = require('fs');
 
 const PUBLIC_PATH = path.resolve(__dirname, 'dist')
 
@@ -10,8 +11,37 @@ const htmlWebpackPluginDefaults = {
   inject: 'head'
 }
 
-const pages = ['404.html', 'admin.html', 'admin-auth.html', 'admin-contest.html', 'admin-empty.html', 'admin-match.html', 'admin-pleyof.html', 'admin-team.html', 'admin-team-detail.html', 'admin-tournir.html', 'aim.html', 'contact.html', 'contest.html', 'contest-detail.html', 'docs.html', 'dude-inner.html', 'index.html', 'lk.html', 'lk-games.html', 'magazin.html', 'new-part.html', 'news.html', 'news-inner.html', 'partners.html', 'photos.html', 'photos-inner.html', 'refereeing.html', 'smi.html', 'tipycal.html', 'ui.html', 'video.html', 'lk-games-empty.html'];
-const dialogs = ['dialog-add--admin.html', 'dialog-add--admin1.html', 'dialog-contest.html', 'dialog-contest-empty.html', 'dialog-form.html', 'dialog-lk-add.html', 'dialog-lk-contest.html', 'dialog-lk-dude.html', 'dialog-lk-edit.html', 'dialog-lk-games.html', 'dialog-match.html', 'dialog-protocol.html', 'dialog-remove--admin.html', 'dialog-remove--lk.html', 'dialog-success.html', 'dialog-success--lk.html', 'dialog-team.html'];
+
+function generateHtmlPlugins(templateDir) {
+  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+  return templateFiles.map(item => {
+    const parts = item.split('.');
+    const name = parts[0];
+    const extension = parts[1];
+    if (extension !== 'html') return null;
+    if (name.includes('dialog')) return null;
+
+    return new HtmlWebpackPlugin({
+      ...htmlWebpackPluginDefaults,
+      filename: `${name}.html`,
+      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+
+    })
+  }).filter((item) => item !== null)
+}
+
+function generateCopyDialogsPlugins(templateDir) {
+  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+  return templateFiles.map(item => {
+    const parts = item.split('.');
+    const name = parts[0];
+    const extension = parts[1];
+    if (extension !== 'html') return null;
+    if (!name.includes('dialog')) return null;
+
+    return { from: `${templateDir}/${name}.${extension}`, to: `./${name}.${extension}` }
+  }).filter((item) => item !== null)
+}
 
 module.exports = {
   entry: './src/index.js',
@@ -53,33 +83,21 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
-      }
+      },
+      {
+        test: /\.html$/,
+        include: path.resolve(__dirname, 'src/includes'),
+        use: ['raw-loader']
+      },
     ],
   },
   plugins: [
+    ...generateHtmlPlugins('./src'),
     new MiniCssExtractPlugin({ filename: "css/style.css", }),
-    ...pages.map((name) => new HtmlWebpackPlugin({
-      ...htmlWebpackPluginDefaults,
-      template: `./src/${name}`,
-      filename: name
-    })),
-    // new HtmlWebpackPlugin({
-    //   ...htmlWebpackPluginDefaults,
-    //   template: './src/index.html',
-    //   filename: 'index.html'
-    // }),
-    // new HtmlWebpackPlugin({
-    //   ...htmlWebpackPluginDefaults,
-    //   template: './src/ui.html',
-    //   filename: 'ui.html'
-    // }),
     new CopyPlugin({
       patterns: [
         { from: "./src/img/", to: "./img/" },
-        ...dialogs.map((name) => ({ from: `./src/${name}`, to: `./${name}` }))
-        // { from: "./src/dialog-small.html", to: "./dialog-small.html" },
-        // { from: "./src/dialog-middle.html", to: "./dialog-middle.html" },
-        // { from: "./src/dialog-large.html", to: "./dialog-large.html" },
+        ...generateCopyDialogsPlugins('./src')
       ],
     }),
   ],
